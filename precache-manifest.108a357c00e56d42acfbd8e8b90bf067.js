d7f95a0f01e724efa7a8eee61fa89daa28de27e9
margarita <margarita@MacBook-Air-Admin.local>
2019-03-17 14:18:12 +0300

    Updates

---
+++ precache-manifest.108a357c00e56d42acfbd8e8b90bf067.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tvshows-react-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5e02452229af8f26465e",
+    "url": "/tvshows-react-app/static/js/runtime~main.de279a21.js"
+  },
+  {
+    "revision": "d90841ea3b96ad178b18",
+    "url": "/tvshows-react-app/static/js/main.9d50468c.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/tvshows-react-app/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "d90841ea3b96ad178b18",
+    "url": "/tvshows-react-app/static/css/main.50d86483.chunk.css"
+  },
+  {
+    "revision": "562657aa383e907eff5fa8fe69472d00",
+    "url": "/tvshows-react-app/index.html"
+  }
+];
